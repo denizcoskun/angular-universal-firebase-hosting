e7d9d02ac4be8dafc8e7bbc07232415dfd5fb2e4
cos <c.deniz@gmail.com>
2017-07-30 16:53:40 +0300

    corrections

--- functions/index.js
+++ functions/index.js
@@ -2,18 +2,20 @@
 const functions = require('firebase-functions');
 const express = require('express');
 require('zone.js/dist/zone-node');
+const path = require('path')
+
 
 
 
 // Import renderModuleFactory from @angular/platform-server.
-var renderModuleFactory = require('@angular/platform-server').renderModuleFactory;
+const renderModuleFactory = require('@angular/platform-server').renderModuleFactory;
 
 // Import the AOT compiled factory for your AppServerModule.
 // This import will change with the hash of your built server bundle.
-var AppServerModuleNgFactory = require('./dist-server/main.61c8194551162576f43c.bundle').AppServerModuleNgFactory;
+const AppServerModuleNgFactory = require('./dist-server/main.61c8194551162576f43c.bundle').AppServerModuleNgFactory;
 
 // Load the index.html file.
-var index = require('fs').readFileSync('./dist-server/index.html', 'utf8');
+const index = require('fs').readFileSync(path.resolve(__dirname, './dist-server/index.html'), 'utf8');
 
 // Render to HTML and log it to the console.
 

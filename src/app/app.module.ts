ec444ffc02e9f5bfd160d874473ab5e5cb1e2d53
cos <c.deniz@gmail.com>
2017-07-30 15:29:46 +0300

    universal enabled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({appId: 'my-app'}),
   ],
   providers: [],
   bootstrap: [AppComponent]
